57d0b1ee75dcac4a118cc3508fde723287ac0d47
SAKTHI k <ss5095757@gmail.com>
2026-06-08 20:28:08 +0530

    Add files via upload

---
+++ theater.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+class theater{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+       
+    char[][]seat=new char[8][8];
+       for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+          seat[i][j]='#';
+        }
+       }
+      
+     while(true){
+        System.out.println("Seats Availability");
+           System.out.println();
+            for(int i=0;i<8;i++){
+              for(int j=0;j<8;j++){
+                 System.out.print(seat[i][j]+" ");
+               }
+              System.out.println();
+            }
+      System.out.println();
+
+   System.out.println("Select the Row");
+   
+     int row=sc.nextInt();
+     System.out.println("select the seats");
+     int col=sc.nextInt();
+           
+      if(row<1 || row>8 ||col<1 || col>8){
+         System.out.println("Invalid seats");
+         continue;
+      }
+ 
+     if(seat[row-1][col-1]=='1'){
+        System.out.println("Seat is already booked");
+    }
+    else{
+       seat[row-1][col-1]='1';
+        System.out.println("Seat booked Succeccfully");
+   }
+
+    System.out.print("Book another ticket ?:");
+    System.out.println("yes/no");
+     char  choice=sc.next().charAt(0);
+
+      if(choice=='n'|| choice=='N')
+       break;
+       }
+   System.out.println("Your seat is:");
+     for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+          System.out.print(seat[i][j]+" ");
+       }
+      System.out.println();  
+    }
+}
+}
